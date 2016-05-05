227db3477c108dd12ce9faae6faa88989ef75f0f
Jan Plhak <jenda.plhak@gmail.com>
2016-05-05 10:47:54 +0200

    interleave finished

--- interleave.cpp
+++ interleave.cpp
@@ -4,59 +4,6 @@
 #include <string>
 #include <vector>
 
-// template< class I >
-// class cyclic_iterator
-//  /* : public iterator< bidirectional, yadda yadda > */ {
-//     I it, beg, end;
-//     int cnt;
-//     cyclic_iterator( int c, I f, I l )
-//         : it( f ), beg( f ), end( l ), cnt( c ) {}
-// public:
-//     cyclic_iterator() : it(), beg(), end(), cnt() {}
-
-//     cyclic_iterator &operator++() {
-//         ++it;
-//         if ( it == end ) {
-//             ++cnt;
-//             it = beg;
-//         }
-//     } // etc for --, post-operations
-
-//     friend bool operator==
-//         ( cyclic_iterator const &lhs, cyclic_iterator const &rhs )
-//         { return lhs.it == rhs.it && lhs.cnt == rhs.cnt; } // etc for !=
-
-//     friend pair< cyclic_iterator, cyclic_iterator > cycle_range
-//         ( int c, I f, I l ) {//factory function, better style outside this scope
-//         return make_pair( cyclic_iterator( 0, f, l ),
-//                           cyclic_iterator( c, f, l ) );
-//     }
-// };
-
-class Search {
-private:
-    std::string m_str;
-    std::string m_pat1;
-    std::string m_pat2;
-public:
-    Search(const std::string& str, const std::string& pat1,
-        const std::string& pat2)
-     : m_str(str), m_pat1(pat1), m_pat2(pat2)
-    { }
-
-    bool rec(size_t i, size_t j, size_t k) {
-        // Base Case: If all strings are empty
-        if (i >= m_str.size()) {
-            return true;
-        }
-
-        // If any of the above mentioned two possibilities is true,
-        // then return true, otherwise false
-        return ( (m_str.at(i)   == m_pat1.at(j % m_pat1.size())) && this->rec(i + 1, j + 1, k))
-               || ((m_str.at(i) == m_pat2.at(k % m_pat2.size())) && this->rec(i + 1, j, k + 1));
-    }
-};
-
 void dump(const std::vector<std::vector<bool>>& array)
 {
     for (const auto& row : array) {
@@ -83,54 +30,44 @@ public:
 // A simple recursive function to check whether C is an interleaving of A and B
 bool jeSpojenim(std::string str, std::string pat1_str, std::string pat2_str)
 {
-    Search S(str, pat1_str, pat2_str );
-    S.rec(0, 0, 0);
+    if (pat1_str.empty() && pat2_str.empty()) {
+        return str.empty();
+    }
+    if (pat1_str.empty()) {
+        std::swap(pat1_str, pat2_str);
+    }
 
     CycleStr pat1(pat1_str);
-    CycleStr pat2(pat2_str);
+    // Simple when one of patterns is empty
+    if (pat2_str.empty()) {
+        for (size_t i = 0; i < str.size(); ++i) {
+            if (str[i] != pat1[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
 
+    CycleStr pat2(pat2_str);
     size_t size = str.size() + 1;
     std::vector<std::vector<bool>> IL(size, std::vector<bool>(size, false));
 
-    for (size_t i = 0; i < size; ++i) {
-        for (size_t j = 0; j + i < size; ++j) {
-            // two empty strings have an empty string as interleaving
-            if (i == 0 && j == 0) {
-                IL[i][j] = true;
-            }
-            // Nothing chosen from pat1
-            else if (i == 0 && pat2[j - 1] == str[j - 1]) {
-                IL[i][j] = IL[i][j - 1];
-            }
-            // Nothing chosen from pat2
-            else if (j == 0 && pat1[i - 1] == str[i - 1]) {
-                IL[i][j] = IL[i-1][j];
-            }
+    IL[0][0] = true;
+    for (size_t i = 1; i < size; ++i) {
+        // Nothing chosen from pat1
+        IL[0][i] = IL[0][i-1] && pat2[i-1] == str[i-1];
+        // Nothing chosen from pat2
+        IL[i][0] = IL[i-1][0] && pat1[i-1] == str[i-1];
+    }
 
-            if (j == 0 || i == 0) {
-                continue;
-            }
+    for (size_t i = 1; i < size; ++i) {
+        for (size_t j = 1; j + i < size; ++j) {
             char c = str[i + j - 1];
-            IL[i][j] = IL[i-1][j] && (pat1[i-1] == c || pat2[j - 1] == c);
-
-            // // Current character of str matches with current character of pat1,
-            // // but doesn't match with current character of pat2
-            // else if(pat1[i-1] == str[i + j - 1] && pat2[j - 1] != str[i + j - 1]) {
-            //     IL[i][j] = IL[i-1][j];
-            // }
-            // // Current character of C matches with current character of pat2,
-            // // but doesn't match with current character of pat1
-            // else if (pat1[i - 1] != str[i + j - 1] && pat2[j-1] == str[i + j - 1]) {
-            //     IL[i][j] = IL[i][j-1];
-            // }
-
-            // // Current character of C matches with that of both pat1 and pat2
-            // else if (pat1[i - 1] == str[i + j - 1] && pat2[j - 1] == str[i + j - 1]) {
-            //     IL[i][j] = IL[i-1][j] || IL[i][j-1];
-            // }
+            IL[i][j] = (IL[i-1][j] && pat1[i-1] == c)
+                    || (IL[i][j-1] && pat2[j-1] == c);
         }
     }
-    dump(IL);
+    // dump(IL);
     for (size_t i = 0; i < size; ++i) {
         if (IL[i][size - 1 - i]) {
             return true;
@@ -143,11 +80,8 @@ bool jeSpojenim(std::string str, std::string pat1_str, std::string pat2_str)
 int main(int argc, char const *argv[])
 {
     auto is_interleave = [](const auto& str, const auto& pat1, const auto& pat2) {
-        Search S(str, pat1, pat2);
-        assert(jeSpojenim(str, pat1, pat2) == S.rec(0, 0, 0));
-        std::cout << jeSpojenim(str, pat1, pat2) << std::endl;;
-        return S.rec(0, 0, 0);
+        return jeSpojenim(str, pat1, pat2);
     };
-    std::cout << is_interleave("SLW.", "SLOVO", "WORD") << std::endl;
+    std::cout << is_interleave("", "", "") << std::endl;
     return 0;
 }
